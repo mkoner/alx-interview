8a566a79a27a74cfc75a0424ecb0214c086d34dd
mkoner <amzid@hotmail.fr>
2023-03-10 00:02:02 +0000

    again

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api';
+const SW_URL = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  request(`${SW_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+    const characters = JSON.parse(body).characters;
+    const names = characters.map(
       url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
+        request(url, (promiseErr, __, body) => {
           if (promiseErr) {
             reject(promiseErr);
           }
-          resolve(JSON.parse(charactersReqBody).name);
+          resolve(JSON.parse(body).name);
         });
       }));
 
-    Promise.all(charactersName)
+    Promise.all(names)
       .then(names => console.log(names.join('\n')))
       .catch(allErr => console.log(allErr));
   });
