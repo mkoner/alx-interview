7afde79d6bb5e2595e5febdb91339a3de9c3b8c3
mkoner <amzid@hotmail.fr>
2023-03-17 01:49:38 +0000

    Pycode style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,18 +3,18 @@
 
 
 def rotate_2d_matrix(matrix):
-  '''
-  Function to rotate a matrix
-  Args:
-    matrix (list[[list]]): a matrix
-  '''
-  n = len(matrix)
-  for i in range(int(n / 2)):
-    y = (n - i - 1)
-    for j in range(i, y):
-      x = (n - 1 - j)
-      tmp = matrix[i][j]
-      matrix[i][j] = matrix[x][i]
-      matrix[x][i] = matrix[y][x]
-      matrix[y][x] = matrix[j][y]
-      matrix[j][y] = tmp
+    '''
+    Function to rotate a matrix
+    Args:
+      matrix (list[[list]]): a matrix
+    '''
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[y][x]
+            matrix[y][x] = matrix[j][y]
+            matrix[j][y] = tmp
