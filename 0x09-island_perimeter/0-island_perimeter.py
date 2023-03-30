84097976ceea07c8a761aaea6fcd4f12f03b53dc
mkoner <amzid@hotmail.fr>
2023-03-30 13:48:26 +0000

    Task1

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Island Perimeter
+'''
+def island_perimeter(grid):
+    '''Function  that returns the perimeter of the island 
+    described in grid
+    '''
+    A = set()
+    B = set()
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i not in A:
+                    A.add(i)
+                    perimeter+=2
+                if j not in B:
+                    B.add(j)
+                    perimeter+=2
+    return perimeter
