5e22d19fe4b6297db87e0c9fe1e78f4512a1840e
mkoner <amzid@hotmail.fr>
2023-03-30 14:27:29 +0000

    Task1 again a

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,7 +3,7 @@
 Island Perimeter
 '''
 def island_perimeter(grid):
-    '''Function  that returns the perimeter of the island 
+    '''Function  that returns the perimeter of the island
     described in grid
     '''
     if type(grid) != list:
@@ -16,8 +16,8 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 if i not in A:
                     A.add(i)
-                    perimeter+=2
+                    perimeter += 2
                 if j not in B:
                     B.add(j)
-                    perimeter+=2
+                    perimeter += 2
     return perimeter
