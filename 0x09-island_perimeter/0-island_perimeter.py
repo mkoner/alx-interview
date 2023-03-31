20ed0720709a072bad1ed3c1077068049d640014
mkoner <amzid@hotmail.fr>
2023-03-31 15:17:46 +0000

    Corrected

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,9 @@ Island Perimeter
 '''
 
 
+from sqlalchemy import null
+
+
 def island_perimeter(grid):
     '''
     Function  that calculates the perimeter of the island
@@ -17,13 +20,23 @@ def island_perimeter(grid):
     A = set()
     B = set()
     perimeter = 0
+    prevI = -1
+    prevJ = -1
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if i not in A:
                     A.add(i)
                     perimeter += 2
+                else:
+                    if prevJ != -1 and j - prevJ > 1:
+                        perimeter += 2
                 if j not in B:
                     B.add(j)
                     perimeter += 2
+                else:
+                    if prevI != -1 and i - prevI > 1:
+                        perimeter += 2
+                prevJ = j
+                prevI = i
     return perimeter
