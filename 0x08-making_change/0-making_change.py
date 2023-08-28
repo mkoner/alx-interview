85862cf3a93b4a143446fed5513b6f22d1475f1e
mkoner <amzid@hotmail.fr>
2023-08-28 01:20:28 +0000

    Correction to meet pep8 requirements

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,21 +7,27 @@ def makeChange(coins, total):
     makeChange function
     Given a pile of coins of different values, determine the fewest
     number of coins needed to meet a given amount total
+
     Parameters:
     -coins: list of the values of the coins in your possession
     -total: given amount to be met
+
     Returns:
     fewest number of coins needed to meet total
     -If total is 0 or less, return 0
-    -If total cannot be met by any number of coins you have, 
+    -If total cannot be met by any number of coins you have,
     return -1
     """
+
     if not coins or coins is None:
         return -1
+
     if total <= 0:
         return 0
+
     change = 0
     coins = sorted(coins)[::-1]
+
     for coin in coins:
         while coin <= total:
             total -= coin
